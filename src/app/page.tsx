d064f1b2b258d022e7b05f07f1f6062b65c41e86
vishal <vishalk.23cse@kongu.edu>
2026-09-02 12:16:12 +0530

    Update title under Karunya S to Founder & CEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1314,7 +1314,7 @@ export default function HomePage() {
               </div>
               <div className="space-y-0.5 pt-0.5">
                 <h4 className="font-black text-lg sm:text-xl text-[#002137]">Karunya S</h4>
-                <p className="text-xs sm:text-sm font-extrabold text-[#b89047]">Founder</p>
+                <p className="text-xs sm:text-sm font-extrabold text-[#b89047]">Founder &amp; CEO</p>
               </div>
             </div>
           </div>
